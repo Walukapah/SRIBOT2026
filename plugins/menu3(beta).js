6b91f1d9d053711f629d6b8c40fe80bfb329ce24
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-09-15 15:31:15 +0530

    Update menu3(beta).js

--- plugins/menu3(beta).js
+++ plugins/menu3(beta).js
@@ -1,7 +1,6 @@
 const config = require('../config');
 const { cmd, commands } = require('../command');
-const pkg = require("@whiskeysockets/baileys");
-const { proto, generateWAMessageFromContent } = pkg;
+const { proto, generateWAMessageFromContent } = require('@whiskeysockets/baileys');
 
 cmd({
     pattern: "menu3",
@@ -9,106 +8,32 @@ cmd({
     category: "utility",
     filename: __filename
 },
-async(conn, mek, m, { from }) => {
+async(conn, mek, m, { from, reply }) => {
     try {
-        // Generate the interactive message
-        const msg = generateWAMessageFromContent(from, {
-            viewOnceMessage: {
-                message: {
-                    messageContextInfo: {
-                        deviceListMetadata: {},
-                        deviceListMetadataVersion: 2
-                    },
-                    interactiveMessage: proto.Message.InteractiveMessage.create({
-                        body: proto.Message.InteractiveMessage.Body.create({
-                            text: "PRABATH-MD-BETA_PUBLIC\n+234 816 597 5051, +254 799 073744, +263 78 0...\n\nShare: 451\nViews: 428656"
-                        }),
-                        footer: proto.Message.InteractiveMessage.Footer.create({
-                            text: "Select an option below"
-                        }),
-                        header: proto.Message.InteractiveMessage.Header.create({
-                            title: "MAIN MENU",
-                            hasMediaAttachment: false
-                        }),
-                        nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-                            buttons: [
-                                {
-                                    name: "quick_reply",
-                                    buttonParamsJson: JSON.stringify({
-                                        display_text: "AI MENU",
-                                        id: ".aimenu"
-                                    })
-                                },
-                                {
-                                    name: "quick_reply",
-                                    buttonParamsJson: JSON.stringify({
-                                        display_text: "SEARCH MENU",
-                                        id: ".searchmenu"
-                                    })
-                                },
-                                {
-                                    name: "quick_reply",
-                                    buttonParamsJson: JSON.stringify({
-                                        display_text: "DOWNLOAD MENU",
-                                        id: ".downloadmenu"
-                                    })
-                                },
-                                {
-                                    name: "quick_reply",
-                                    buttonParamsJson: JSON.stringify({
-                                        display_text: "OWNER MENU",
-                                        id: ".ownermenu"
-                                    })
-                                },
-                                {
-                                    name: "quick_reply",
-                                    buttonParamsJson: JSON.stringify({
-                                        display_text: "CONVERT MENU",
-                                        id: ".convertmenu"
-                                    })
-                                },
-                                {
-                                    name: "quick_reply",
-                                    buttonParamsJson: JSON.stringify({
-                                        display_text: "GROUP MENU",
-                                        id: ".groupmenu"
-                                    })
-                                },
-                                {
-                                    name: "quick_reply",
-                                    buttonParamsJson: JSON.stringify({
-                                        display_text: "STICKER MENU",
-                                        id: ".stickermenu"
-                                    })
-                                },
-                                {
-                                    name: "quick_reply",
-                                    buttonParamsJson: JSON.stringify({
-                                        display_text: "GAME MENU",
-                                        id: ".gamemenu"
-                                    })
-                                },
-                                {
-                                    name: "quick_reply",
-                                    buttonParamsJson: JSON.stringify({
-                                        display_text: "MATHTOOL MENU",
-                                        id: ".mathtoolmenu"
-                                    })
-                                }
-                            ]
-                        })
-                    })
-                }
-            }
-        }, {});
+        const buttons = [
+            { buttonId: '.aimenu', buttonText: { displayText: 'AI MENU' }, type: 1 },
+            { buttonId: '.searchmenu', buttonText: { displayText: 'SEARCH MENU' }, type: 1 },
+            { buttonId: '.downloadmenu', buttonText: { displayText: 'DOWNLOAD MENU' }, type: 1 },
+            { buttonId: '.ownermenu', buttonText: { displayText: 'OWNER MENU' }, type: 1 },
+            { buttonId: '.convertmenu', buttonText: { displayText: 'CONVERT MENU' }, type: 1 },
+            { buttonId: '.groupmenu', buttonText: { displayText: 'GROUP MENU' }, type: 1 },
+            { buttonId: '.stickermenu', buttonText: { displayText: 'STICKER MENU' }, type: 1 },
+            { buttonId: '.gamemenu', buttonText: { displayText: 'GAME MENU' }, type: 1 },
+            { buttonId: '.mathtoolmenu', buttonText: { displayText: 'MATHTOOL MENU' }, type: 1 }
+        ];
 
-        // Send the message
-        await conn.relayMessage(msg.key.remoteJid, msg.message, {
-            messageId: msg.key.id
-        });
+        const buttonMessage = {
+            text: "SRI BOT",
+            footer: "Select an option below",
+            headerType: 1,
+            buttons: buttons,
+            viewOnce: true
+        };
+
+        await conn.sendMessage(from, buttonMessage, { quoted: mek });
 
     } catch (error) {
         console.error('Menu3 error:', error);
-        await conn.sendMessage(from, { text: "Failed to display menu. Please try again." }, { quoted: mek });
+        await reply("Failed to display menu. Please try again.");
     }
 });
