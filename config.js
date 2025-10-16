4c31048f9b0de2dc3f6ea0b264cda02636a3d7fa
Walukapah <90122948+Walukapah@users.noreply.github.com>
2025-10-16 14:29:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
     PREFIX: process.env.PREFIX || ".",
     BOT_NAME: process.env.BOT_NAME || "SRI-BOT 🇱🇰",
     BOT_INFO: process.env.BOT_INFO || "SRI-BOT🇱🇰;WALUKA👊;https://i.imgur.com/r3GZeiX.jpeg",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || ["94753670175"],
+    OWNER_NUMBER: process.env.OWNER_NUMBER || ["94728115797"],
     ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg",
     ALIVE_MSG: process.env.ALIVE_MSG || "iyoo whats up 💫",
     MENU_IMG_URL: process.env.MENU_IMG_URL || "https://images.weserv.nl/?url=i.imgur.com/W2CaVZW.jpeg",
@@ -23,7 +23,7 @@ module.exports = {
     MODE: process.env.MODE || "public", // bot modes (public,private,inbox,groups)
     VERSION: process.env.VERSION || "1.0.0", // Added version here
     MEDIA_URL: process.env.MEDIA_URL || "https://whatsapp.com/channel/0029VaAPzWX0G0XdhMbtRI2i",
-    AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
+    AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
     ANTI_DELETE: process.env.ANTI_DELETE || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true"
 };
